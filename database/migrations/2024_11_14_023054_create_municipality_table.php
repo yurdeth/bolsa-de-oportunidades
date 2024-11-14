1cbbd66422bad76132d6caaaca994a7ba0b20303
Alex Ulloa <US21003@ues.edu.sv>
2024-11-13 21:04:43 -0600

    Datos iniciales para usuarios

--- database/migrations/2024_11_14_023054_create_municipality_table.php
+++ database/migrations/2024_11_14_023054_create_municipality_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('sv_municipality', function (Blueprint $table) {
             $table->id();
+            $table->string('municipality_name');
+            $table->foreignId('sv_department_id')
+                ->constrained('sv_departments')
+                ->cascadeOnUpdate()
+                ->cascadeOnUpdate();
             $table->timestamps();
         });
     }
@@ -20,8 +23,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('municipality');
     }
 };
--- database/migrations/2024_11_14_023101_create_districts_table.php
+++ database/migrations/2024_11_14_023101_create_districts_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('sv_districts', function (Blueprint $table) {
             $table->id();
+            $table->string('district_name');
+            $table->foreignId('municipality_id')
+                ->constrained('sv_municipalitiy')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
             $table->timestamps();
         });
     }
@@ -20,8 +23,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('districts');
     }
 };
