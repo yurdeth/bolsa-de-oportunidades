a50371dbb751b4a42a327ca3cf208c9352ef794e
Alex Ulloa <US21003@ues.edu.sv>
2024-11-15 20:25:55 -0600

    Documentación StudentController

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -7,14 +7,28 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class StudentsController extends Controller {
-    public function index() {
+    /**
+     * Muestra una lista de los estudiantes.
+     *
+     * Este método verifica si el usuario autenticado tiene el rol de administrador principal ('1')
+     * o coordinador ('2'). Si no es así, redirige al usuario a la página de inicio.
+     * Luego, obtiene la información de los estudiantes utilizando el método `getStudentsInfo`
+     * del modelo `User`. Si no hay estudiantes registrados, devuelve una respuesta JSON con
+     * un mensaje de error y un código de estado 404. Si hay estudiantes registrados, devuelve
+     * una respuesta JSON con la información de los estudiantes y un código de estado 201.
+     *
+     * @return JsonResponse Respuesta JSON con la información de los estudiantes
+     * o un mensaje de error.
+     */
+    public function index(): JsonResponse {
         if (Auth::user()->rol_id != '1' && Auth::user()->rol_id != '2') {
-            return redirect()->route('inicio');
+            return response()->json([
+                'success' => false,
+                'message' => 'Esta ruta no existe' // <- Para no dar la pista de que la ruta es verdadera y evitar fallas de seguridad
+            ], 404);
         }
 
         $students = (new User())->getStudentsInfo();
@@ -29,10 +43,24 @@ class StudentsController extends Controller {
         return response()->json([
             'success]' => true,
             'students' => $students,
-            ], 201);
+        ], 201);
     }
 
-    public function store(Request $request) {
+    /**
+     * Almacena un nuevo estudiante en la base de datos.
+     *
+     * Este método valida los datos de la solicitud para asegurarse de que todos los campos requeridos
+     * estén presentes y sean válidos. Si la validación falla, devuelve una respuesta JSON con los
+     * errores de validación y un código de estado 422. Si la validación es exitosa, crea un nuevo
+     * usuario y un nuevo estudiante en la base de datos, inicia sesión con el nuevo usuario y
+     * devuelve un token de acceso en la respuesta JSON con un código de estado 201. Si ocurre un
+     * error durante el proceso, devuelve una respuesta JSON con un mensaje de error y un código
+     * de estado 500.
+     *
+     * @param Request $request La solicitud HTTP que contiene los datos del estudiante.
+     * @return JsonResponse Respuesta JSON con el resultado de la operación.
+     */
+    public function store(Request $request): JsonResponse {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string',
             'carnet' => 'required|string|unique:students,carnet',
@@ -77,7 +105,20 @@ class StudentsController extends Controller {
         }
     }
 
-    public function show($id) {
+    /**
+     * Muestra la información de un estudiante específico.
+     *
+     * Este método verifica si el usuario autenticado tiene permiso para ver la información del estudiante.
+     * Los permisos se limitan al propio usuario, al administrador principal ('1') y al coordinador ('2').
+     * Si el usuario no tiene permiso, se redirige a la página de inicio.
+     * Luego, obtiene la información del estudiante utilizando el método `getStudentsInfoById` del modelo `User`.
+     * Si el estudiante no se encuentra, devuelve una respuesta JSON con un mensaje de error y un código de estado 404.
+     * Si el estudiante se encuentra, devuelve una respuesta JSON con la información del estudiante y un código de estado 200.
+     *
+     * @param int $id El ID del estudiante.
+     * @return JsonResponse Respuesta JSON con la información del estudiante o un mensaje de error.
+     */
+    public function show(int $id): JsonResponse {
         /* Limitar visualizacion:
             1. El usuario mismo
             2. El admin principal ('1')
@@ -86,7 +127,10 @@ class StudentsController extends Controller {
         if (Auth::user()->id != $id &&
             Auth::user()->rol_id != '1' &&
             Auth::user()->rol_id != '2') {
-            return redirect()->route('inicio');
+            return response()->json([
+                'success' => false,
+                'message' => 'Esta ruta no existe' // <- Para no dar la pista de que la ruta es verdadera y evitar fallas de seguridad
+            ], 404);
         }
 
         $student = (new User())->getStudentsInfoById($id);
@@ -103,13 +147,32 @@ class StudentsController extends Controller {
         ]);
     }
 
-    public function update(Request $request, $id) {
+    /**
+     * Actualiza la información de un estudiante específico.
+     *
+     * Este método verifica si el usuario autenticado tiene permiso para actualizar la información del estudiante.
+     * Los permisos se limitan al propio usuario y al administrador principal ('1'). Si el usuario no tiene permiso,
+     * se redirige a la página de inicio. Luego, busca al usuario y al estudiante en la base de datos.
+     * Si ambos existen, valida los datos de la solicitud. Si la validación falla, devuelve una respuesta JSON
+     * con los errores de validación y un código de estado 422. Si la validación es exitosa, actualiza la información
+     * del usuario y del estudiante en la base de datos y devuelve una respuesta JSON con la información actualizada
+     * del estudiante y un código de estado 200. Si el estudiante no se encuentra, devuelve una respuesta JSON con
+     * un mensaje de error y un código de estado 404.
+     *
+     * @param Request $request La solicitud HTTP que contiene los datos del estudiante.
+     * @param int $id El ID del estudiante.
+     * @return JsonResponse Respuesta JSON con el resultado de la operación.
+     */
+    public function update(Request $request, int $id): JsonResponse {
         /* Limitar visualizacion:
             1. El usuario mismo
             2. El admin principal ('1')
         */
         if (Auth::user()->id != $id && Auth::user()->roles_id != 1) {
-            return redirect()->route('inicio');
+            return response()->json([
+                'success' => false,
+                'message' => 'Esta ruta no existe' // <- Para no dar la pista de que la ruta es verdadera y evitar fallas de seguridad
+            ], 404);
         }
 
         $user = User::find($id);
@@ -154,13 +217,32 @@ class StudentsController extends Controller {
         ], 404);
     }
 
-    public function partial(Request $request, $id) {
+    /**
+     * Actualiza parcialmente la información de un estudiante específico.
+     *
+     * Este método verifica si el usuario autenticado tiene permiso para actualizar la información del estudiante.
+     * Los permisos se limitan al propio usuario y al administrador principal ('1'). Si el usuario no tiene permiso,
+     * se devuelve una respuesta JSON con un mensaje de error y un código de estado 404. Luego, busca al usuario y
+     * al estudiante en la base de datos. Si el estudiante existe, valida los datos de la solicitud. Si la validación
+     * falla, devuelve una respuesta JSON con los errores de validación y un código de estado 422. Si la validación
+     * es exitosa, actualiza los campos proporcionados del usuario y del estudiante en la base de datos y devuelve
+     * una respuesta JSON con un código de estado 201. Si el estudiante no se encuentra, devuelve una respuesta JSON
+     * con un mensaje de error y un código de estado 404.
+     *
+     * @param Request $request La solicitud HTTP que contiene los datos del estudiante.
+     * @param int $id El ID del estudiante.
+     * @return JsonResponse Respuesta JSON con el resultado de la operación.
+     */
+    public function partial(Request $request, int $id): JsonResponse {
         /* Limitar visualizacion:
             1. El usuario mismo
             2. El admin principal ('1')
         */
         if (Auth::user()->id != $id && Auth::user()->roles_id != 1) {
-            return redirect()->route('inicio');
+            return response()->json([
+                'success' => false,
+                'message' => 'Esta ruta no existe' // <- Para no dar la pista de que la ruta es verdadera y evitar fallas de seguridad
+            ], 404);
         }
 
         $user = User::find($id);
@@ -221,14 +303,29 @@ class StudentsController extends Controller {
         ], 404);
     }
 
-    public function destroy($id) {
+    /**
+     * Elimina un estudiante específico.
+     *
+     * Este método verifica si el usuario autenticado tiene permiso para eliminar al estudiante.
+     * Los permisos se limitan al propio usuario y al administrador principal ('1'). Si el usuario no tiene permiso,
+     * se devuelve una respuesta JSON con un mensaje de error y un código de estado 404. Luego, busca al usuario en la base de datos.
+     * Si el usuario existe y no es el usuario con ID 1, lo elimina y devuelve una respuesta JSON con un mensaje de éxito.
+     * Si el usuario no se encuentra, devuelve una respuesta JSON con un mensaje de error y un código de estado 404.
+     *
+     * @param int $id El ID del estudiante.
+     * @return JsonResponse Respuesta JSON con el resultado de la operación.
+     */
+    public function destroy(int $id): JsonResponse {
         /* Limitar visualizacion:
             1. El usuario mismo
             2. El admin principal ('1')
         */
         if (Auth::user()->id != $id &&
             Auth::user()->rol_id != '1') {
-            return redirect()->route('inicio');
+            return response()->json([
+                'success' => false,
+                'message' => 'Esta ruta no existe' // <- Para no dar la pista de que la ruta es verdadera y evitar fallas de seguridad
+            ], 404);
         }
 
         if ($id != 1) {
