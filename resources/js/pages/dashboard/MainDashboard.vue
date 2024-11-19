c8d4acc006d0b20c55b88f6e91b5e28e7592a8fd
Cristian Pineda <cristianpined4@outlook.com>
2024-11-19 12:39:48 -0600

    fixed bugs

--- resources/js/pages/dashboard/MainDashboard.vue
+++ resources/js/pages/dashboard/MainDashboard.vue
@@ -4,3 +4,44 @@
         <router-link to="/">Ir a Home</router-link>
     </div>
 </template>
+
+<script>
+import { api } from "../../api";
+
+export default {
+    data() {
+        return {
+            loading: false,
+        };
+    },
+    async mounted() {
+        // Verificar si el usuario ya está autenticado
+        this.loading = true;
+        if (localStorage.getItem("token")) {
+            try {
+                let response = await api.post("/access_token", {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                });
+                localStorage.setItem("token", response.data.data.token);
+                localStorage.setItem(
+                    "user",
+                    JSON.stringify(response.data.data.user)
+                );
+                this.loading = false;
+            } catch (error) {
+                localStorage.removeItem("token");
+                localStorage.removeItem("user");
+                this.loading = false;
+                this.$router.push("/login");
+            }
+        } else {
+            this.loading = false;
+            this.$router.push("/login");
+        }
+    },
+};
+</script>
