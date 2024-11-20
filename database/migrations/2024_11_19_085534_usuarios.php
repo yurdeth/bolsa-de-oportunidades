048c3b6edca9a4bd58c73ee399c00a89acb6721e
Alex Ulloa <US21003@ues.edu.sv>
2024-11-20 09:22:11 -0600

    Uso de variables del .env

--- database/migrations/2024_11_19_085534_usuarios.php
+++ database/migrations/2024_11_19_085534_usuarios.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 use App\Models\User;
 
@@ -22,8 +23,8 @@ return new class extends Migration {
         });
 
         User::create([
-            'email' => 'admin@admin.com',
-            'password' => password_hash('admin', PASSWORD_DEFAULT),
+            'email' => env('MANAGER_EMAIL'),
+            'password' => Hash::make(env('MANAGER_PASSWORD')),
             'id_tipo_usuario' => 1,
             'estado_usuario' => true,
         ]);
