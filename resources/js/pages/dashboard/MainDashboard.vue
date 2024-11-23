968d4838267530439276d23929ad8d007b3a1fb2
Cristian Pineda <cristianpined4@outlook.com>
2024-11-23 15:39:55 -0600

    fixed bugs

--- resources/js/pages/dashboard/MainDashboard.vue
+++ resources/js/pages/dashboard/MainDashboard.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
+    <div
+        class="loader"
+        v-if="loading"
+        style="
+            width: 100%;
+            height: 300px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        "
+    >
+        <div
+            class="spinner-border text-danger"
+            role="status"
+            style="width: 100px; height: 100px"
+        >
+            <span class="sr-only">Cargando...</span>
+        </div>
+    </div>
+    <div v-if="!loading">
         <h1>Bienvenido a Dashboard</h1>
         <router-link to="/">Ir a Home</router-link>
     </div>
@@ -45,3 +64,12 @@ export default {
     },
 };
 </script>
+<style>
+.loader {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
