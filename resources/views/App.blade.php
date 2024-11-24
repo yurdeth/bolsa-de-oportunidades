a9857643d494abf237dbe3d8bff7ddd7a7ca6b10
Cristian Pineda <cristianpined4@outlook.com>
2024-11-24 16:20:54 -0600

    se agrego el layout para de dashboad

--- resources/views/App.blade.php
+++ resources/views/App.blade.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bolsa de oportunidades</title>
+    <title>Bolsa de oportunidades | FMO UES</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     {{-- iconos material --}}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     {{-- Roboto font --}}
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
+    {{-- favicon --}}
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Bolsa de oportunidades para estudiantes de la Universidad de El Salvador pertenecientes a la Facultad Multidisciplinaria Oriental" />
+    {{-- Tailwind --}}
+    {{-- Vite --}}
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
