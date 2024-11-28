2bcf1b566173f83e4f45e506a88c1c5825fc18e7
yurdeth <angelvasquez2570@gmail.com>
2024-11-28 01:41:14 -0600

    Revert "production test"
    This reverts commit 0779877635c6a970498b24ab677beac21ac9c985.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-        }
     }
 }
