da894907c4c041b65540618cac9d90b7a7ce3e99
Cristian Pineda <cristianpined4@outlook.com>
2024-11-30 22:36:16 -0600

    fixed bugs in graficas dashboard

--- resources/js/pages/dashboard/MainDashboard.vue
+++ resources/js/pages/dashboard/MainDashboard.vue
@@ -330,13 +330,13 @@ export default {
                 this.numStudents = response.data.totalStudent;
 
                 let estudiantes = response.data.dataProyectosbyStatus,
-                    label = estudiantes.map((item) => item.carrera),
+                    label = estudiantes.map((item) => item.status),
                     data = estudiantes.map((item) => item.total);
                 chart("#estudiantesChart", label, data);
 
                 let dataAplicacionesByStatus =
                     response.data.dataAplicacionesByStatus;
-                label = dataAplicacionesByStatus.map((item) => item.status);
+                label = dataAplicacionesByStatus.map((item) => item.rol);
                 data = dataAplicacionesByStatus.map((item) => item.total);
                 chart("#requestChart", label, data);
             }
