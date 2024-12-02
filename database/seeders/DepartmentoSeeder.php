074a99a74c765224ea07015379fde4d83b4fb94d
Alex Ulloa <US21003@ues.edu.sv>
2024-12-02 11:08:16 -0600

    Seeders-> id en departamentos y carreras para evitar duplicidad

--- database/seeders/DepartmentoSeeder.php
+++ database/seeders/DepartmentoSeeder.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\DB;
 class DepartmentoSeeder extends Seeder {
     public function run(): void {
         $departments = [
-            ['nombre_departamento' => 'Ingeniería y Arquitectura', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Medicina', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Humanidades', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Agronomía', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Economía', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Jurisprudencia', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Ciencias Naturales y Matemática', 'created_at' => now(), 'updated_at' => now()],
-            ['nombre_departamento' => 'Química y Farmacia', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'nombre_departamento' => 'Ingeniería y Arquitectura', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'nombre_departamento' => 'Medicina', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'nombre_departamento' => 'Humanidades', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 4, 'nombre_departamento' => 'Agronomía', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 5, 'nombre_departamento' => 'Economía', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 6, 'nombre_departamento' => 'Jurisprudencia', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 7, 'nombre_departamento' => 'Ciencias Naturales y Matemática', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 8, 'nombre_departamento' => 'Química y Farmacia', 'created_at' => now(), 'updated_at' => now()],
         ];
 
         // Insert all the departments at once
